817f111410235c67f8eacb90791a744f31917273
jousethen <jthen93@gmail.com>
2021-11-25 16:20:40 -0500

    commit tree 0cdc77a0d1fa529714a60655fc9c020c5803f158

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,12 @@
 function findShortestString(arr) {
-  // type your code here
+  let lenArr = arr.map((i) => {
+    return i.length
+  });
+
+  let min = Math.min(...lenArr);
+  let idx = lenArr.indexOf(min);
+
+  return arr[idx];
 }
 
 if (require.main === module) {
@@ -18,9 +25,24 @@ if (require.main === module) {
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
+  // BENCHMARK HERE
+  const startTime = Date.now();
+
+  for (let i = 0; i < 1000; ++i) {
+    findShortestString(['flower', 'juniper', 'lily', 'dadelion']);
+  }
+
+  const avgTime = (Date.now() - startTime) / 1000;
+  console.log(avgTime);
 }
 
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+  Map array to another array of values which are char.length
+  Once we have that, we use min to see which is the lowest then find the index of that
+
+  arr[idx]
+*/
